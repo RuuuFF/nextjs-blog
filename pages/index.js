b0cf9b542e47b3e1fbf4bf7f118cb988e08c2e7b
Carlos Daniel <ruuufffaria@gmail.com>
2022-07-05 20:57:23 -0300

    refactor: classNames from module.css to string

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import Head from "next/head";
 import Link from "next/link";
 import Date from "../components/date";
 import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -22,21 +21,21 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
 
-      <section className={utilStyles.headingMd}>
+      <section className="headingMd">
         <p>Hello! I'm <strong>Carlos</strong>. I'm Frontend Developer and passionate for code, airplane and photograph. You can contact me on <a href="https://www.linkedin.com/in/ruuuff/">LinkedIn</a>.</p>
         <p>(This is a sample website - you'll be building a site like this on <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)</p>
       </section>
 
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
+      <section className="headingMd padding1px">
+        <h2 className="headingLg">Blog</h2>
+        <ul className="list">
           {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
+            <li className="listItem" key={id}>
               <Link href={`/posts/${id}`}>
                 <a>{title}</a>
               </Link>
               <br />
-              <small className={utilStyles.lightText}>
+              <small className="lightText">
                 <Date dateString={date} />
               </small>
             </li>
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,8 @@
 import { getAllPostIds, getPostData } from "../../lib/posts";
+
 import Head from "next/head";
 import Date from "../../components/date";
 import Layout from "../../components/layout";
-import utilStyles from "../../styles/utils.module.css";
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
@@ -29,8 +29,8 @@ export default function Post({ postData }) {
       </Head>
 
       <article>
-        <h1 className={utilStyles.headingX1}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
+        <h1 className="headingX1">{postData.title}</h1>
+        <div className="lightText">
           <Date dateString={postData.date} />
         </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
